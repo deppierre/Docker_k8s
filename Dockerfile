634b08eb1b97460555e4445aea9104b337f34d6d
Pierre Depretz <pierre@depretz.eu>
2020-02-11 13:23:22 +1100

    script connection

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,11 @@ FROM alpine
 ARG redis_password
 ENV REDIS_PORT="12160"
 ENV REDIS_HOST="redis-12160.c51.ap-southeast-2-1.ec2.cloud.redislabs.com"
-ENV REDIS_PASSWORD=redis_password
+ENV REDIS_PASSWORD=${redis_password}
 RUN apk update && \
 #SETUP PACKAGES
     apk add redis && \
     apk add python3 && \
-    pip3 install redis && \
-    git clone https://github.com/deppierre/Docker_k8s.git
+    apk add git && \
+    pip3 install redis --disable-pip-version-check && \
+    cd /tmp && git clone https://github.com/deppierre/Docker_k8s.git
--- import_data.py
+++ import_data.py
@@ -1,15 +1,20 @@
 import redis
 import os
+import sys
 
 try:
     redis_host = os.environ['REDIS_HOST']
     redis_port = os.environ['REDIS_PORT']
     redis_password = os.environ['REDIS_PASSWORD']
-except KeyError as error:
+except KeyError:
     redis_host = 'redis-12160.c51.ap-southeast-2-1.ec2.cloud.redislabs.com'
-    redis_password = 'PdZ0787#'
+    if len(sys.argv) > 1:
+        redis_password = sys.argv[1]
+    else:
+        print('redis_password must be defined')
+        redis_host = redis_port= redis_password = ''
+        sys.exit(0)
 finally:
     redis_port = redis_host.split('.')[0].lstrip('redis-')
+    r = redis.Redis(host=redis_host,port=redis_port,password=redis_password)
 
-
-r = redis.Redis(host=redis_host,port=redis_port,password=redis_password)
